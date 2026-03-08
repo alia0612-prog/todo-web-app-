42ee8797682e9c0b0e889346502ee62124a5c400
Alia Yaqoob <aliayaqoob966@gmail.com>
2026-03-08 17:47:34 +0500

    Initial project structure

---
+++ script.js
@@ -0,0 +1,21 @@
+function addTask(){
+
+let input = document.getElementById("taskInput");
+let task = input.value;
+
+let li = document.createElement("li");
+li.textContent = task;
+
+let delBtn = document.createElement("button");
+delBtn.textContent = "Delete";
+
+delBtn.onclick = function(){
+li.remove();
+}
+
+li.appendChild(delBtn);
+
+document.getElementById("taskList").appendChild(li);
+
+input.value="";
+}
